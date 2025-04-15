40eca2611cd20b38ead45cdade67975999ebb22c
Garin Humpherys <garin.humpherys@students.snow.edu>
2025-04-15 08:50:55 -0600

    moved some code in board.cpp and added soem comments

--- TicTacToe.Logic/Board.cpp
+++ TicTacToe.Logic/Board.cpp
@@ -25,6 +25,7 @@ bool Board::SetSquare(int row, int col, char player)
 		grid[row][col] = player;
 		return true;
 	}
+
 	return false;
 }
 
@@ -32,6 +33,38 @@ char Board::GameStatus()
 {
 	bool won = false;
 	int numChars = 0;
+
+	for (int i = 0; i < 3; i++)
+	{
+		//horizontal win
+		if (grid[0][i] != ' ' && grid[1][i] == grid[0][i] && grid[2][i] == grid[0][i])
+		{
+			won = true;
+			return grid[0][i];
+		}
+
+		//vertical win
+		if (grid[i][0] != ' ' && grid[i][1] == grid[i][0] && grid[i][2] == grid[i][0])
+		{
+			won = true;
+			return grid[i][0];
+		}
+	}
+
+	//diagonal LR win
+	if (grid[0][0] != ' ' && grid[1][1] == grid[0][0] && grid[2][2] == grid[0][0])
+	{
+		won = true;
+		return grid[0][0];
+	}
+
+	//diagonal RL win
+	if (grid[0][2] != ' ' && grid[1][1] == grid[0][2] && grid[2][0] == grid[0][2])
+	{
+		won = true;
+		return grid[0][2];
+	}
+
 	for (int i = 0; i < 3; i++)
 	{
 		for (int j = 0; j < 3; j++)
@@ -41,33 +74,6 @@ char Board::GameStatus()
 				numChars++;
 			}
 		}
-	}
-
-	for (int i = 0; i < 3; i++)
-	{
-		if (grid[0][i] != ' ' && grid[1][i] == grid[0][i] && grid[2][i] == grid[0][i])
-		{
-			won = true;
-			return grid[0][i];
-		}
-
-		if (grid[i][0] != ' ' && grid[i][1] == grid[i][0] && grid[i][2] == grid[i][0])
-		{
-			won = true;
-			return grid[i][0];
-		}
-	}
-
-	if (grid[0][0] != ' ' && grid[1][1] == grid[0][0] && grid[2][2] == grid[0][0])
-	{
-		won = true;
-		return grid[0][0];
-	}
-
-	if (grid[0][2] != ' ' && grid[1][1] == grid[0][2] && grid[2][0] == grid[0][2])
-	{
-		won = true;
-		return grid[0][2];
 	}
 
 	if (numChars == 9 && !won)
